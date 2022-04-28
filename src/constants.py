1b662a1a76be5e4c591410c48514036b5b861520
MrPiink <104043064+MrPiink@users.noreply.github.com>
2022-04-27 22:21:25 -0400

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,7 @@
 from openpyxl import load_workbook  # pip install openpyxl
 
 DRIVER_PATH = "tools/chromedriver.exe"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
 EXCEL_FILE_LOCATION = "search/search.xlsx"
 WB = load_workbook(EXCEL_FILE_LOCATION)
 WS = WB.active
